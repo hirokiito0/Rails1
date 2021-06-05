31b21f15be51455c9de253c3612421e579a23fc6
EC2 Default User <ec2-user@ip-172-31-93-230.ec2.internal>
2021-06-05 11:14:21 +0000

    [finish]3-7_画像投稿_シークレットキー

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ea52404c2f1ac9b1195dcf755b2d2b311997e8a994d66cb895e9cfa78b49511fbd454ba2c09f27142262b9fd739908f8a3496a06f4ad0170fbc7c94eb43f5ad4'
