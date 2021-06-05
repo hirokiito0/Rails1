2e29a1ef198c568efcbd2abfc1aed3424a5af399
EC2 Default User <ec2-user@ip-172-31-93-230.ec2.internal>
2021-06-05 06:23:30 +0000

    [finish]3-4_投稿機能_createアクション追加

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -2,4 +2,21 @@ class TodolistsController < ApplicationController
   def new
     @list=List.new
   end
+  
+  def create
+    # データの新規登録するためのインスタンス作成
+    list = LIst.new(list_params)
+    # データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # トップ画面へリダイレクト
+    redirect_to'/top'
+  end
+  
+  
+  private
+  #ストロングパラメーター
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+  
 end
